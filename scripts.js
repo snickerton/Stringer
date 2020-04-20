e1e36a4c4a07a6d0cf956612d20f0eb58834449f
Tony Zhu <snickerton@users.noreply.github.com>
2020-04-19 21:13:26 -0500

    reformatted functions and removed output box, added edit history?

--- scripts.js
+++ scripts.js
@@ -1,25 +1,30 @@
+var input = document.getElementById("input").value;
+var output = "";
+
 function insertCharacters() {
+var input = document.getElementById("input").value;
+var output = "";
+
     var charInsert = document.getElementById("character_insert").value;
     var indexInsert = document.getElementById("index_insert").value;
-    var input = document.getElementById("input").value;
-    var output = "";
 
-    for(var i = 0;i<input.length;i++){
+    for (var i = 0; i < input.length; i++) {
         output += input[i];
-        if(i%indexInsert==indexInsert-1){
-            output+=charInsert
+        if (i % indexInsert == indexInsert - 1) {
+            output += charInsert
         }
         // console.log(input[i]);
     }
 
-    document.getElementById("output").value = output;
+    document.getElementById("input").value = output;
 }
 
 function replace() {
+var input = document.getElementById("input").value;
+var output = "";
+
     var rOld = document.getElementById("replace_old").value;
     var rNew = document.getElementById("replace_new").value;
-    var input = document.getElementById("input").value;
-    var output = "Hello World!";
 
     //var isRegex = document.getElementById("isRegex").checked;
 
@@ -31,10 +36,52 @@ function replace() {
 
     output = input.replace(rOld, rNew)
 
-    if(output == input){
+    if (output == input) {
         alert("\"" + rOld + "\" not found in original string. :(");
     }
 
-    document.getElementById("output").value = output;
+    document.getElementById("input").value = output;
+
+}
+
+function reverseStr() {
+var input = document.getElementById("input").value;
+var output = "";
+
+    output = input.split("").reverse().join("");
+
+    var node = document.createTextNode(output);
+    var wrapper = document.createElement("p"); 
+    wrapper.appendChild(node)
+    document.getElementById("history").appendChild(wrapper);
+
+    document.getElementById("input").value = output;
+}
+
+function upper() {
+var input = document.getElementById("input").value;
+var output = "";
+
+    output = input.trim();
+
+    document.getElementById("input").value = output;
+}
+
+function lower() {
+var input = document.getElementById("input").value;
+var output = "";
+
+    output = input.trim();
+
+    document.getElementById("input").value = output;
+}
+
+function trimbutnotjusttrimbecausethatnameisntallowedforsomereason() {
+var input = document.getElementById("input").value;
+var output = "";
+
+    output = input.trim();
+    // alert(output.trim())
+    document.getElementById("input").value = output;
 
 }
